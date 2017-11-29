3d6927da294441fb4d0db4467899545d88130cf8
Gaspare Sganga <mail@gasparesganga.com>
2017-11-29 22:36:00 +0100

    .

--- _deploy/deploy.sh
+++ _deploy/deploy.sh
@@ -6,15 +6,15 @@ bundle exec jekyll build
 cd _site
 ls -lR
 
-echo "htaccess_static contents:";
-cat .htaccess_static
-
+echo "";
 echo "htaccess contents:";
 cat .htaccess
 
+echo "";
 echo "email.html contents:";
 cat email.html
 
+echo "";
 echo "loop.html contents:";
 cat loop.html
 
